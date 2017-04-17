db741e044e43a34fa29de585a9e5a2faf6027b06
NAlex2004 <nalex2004@gmail.com>
2017-04-17 17:19:16 +0300

    ...

---
+++ Airlines/Flights/FlightParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAlex.Airlines.Flights
+{
+    public struct FlightParams
+    {
+        int FlightRange;
+        int PassgengersCount;
+        int Cargo;
+    }
+}
--- Airlines/Planes/Plane.cs
+++ Airlines/Planes/Plane.cs
@@ -9,19 +9,45 @@ namespace NAlex.Airlines.Planes
     public abstract class Plane
     {
         private double fuelCount;
+        private int flightRange = 1;
+        private double fuelTankSize = 1;
 
         public int? Id { get; protected set; }
         public string Number { get; set; }
         public string Manufacture { get; protected set; }
         /// <summary>
-        /// Fuel consumption litres per 100 km
+        /// Fuel consumption liters per 100 km
         /// </summary>
-        public double FuelConsumption { get; protected set; }
+        public double FuelConsumption 
+        {
+            get
+            {
+                return 100 * FuelTankSize / FlightRange;
+            }
+        }
         /// <summary>
-        /// Flight range, km
+        /// Flight range, km. Greater or equal 1.
         /// </summary>
-        public int FlightRange { get; protected set; }
-        public double FuelTankSize { get; protected set; }
+        public int FlightRange 
+        {
+            get { return flightRange; }
+            protected set
+            {
+                flightRange = Math.Max(1, value);
+            }
+        }
+        /// <summary>
+        /// Fuel tank size, liters. Greater or equal 1.
+        /// </summary>
+        public double FuelTankSize 
+        {
+            get { return fuelTankSize; }
+            protected set
+            {
+                fuelTankSize = Math.Max(1, value);
+            }
+        }
+
         public double FuelCount 
         {
             get;
@@ -29,6 +55,14 @@ namespace NAlex.Airlines.Planes
             {
                 fuelCount = Math.Min(Math.Max(0, value), FuelTankSize);
             }
+        }
+
+        public Plane(int flightRange, int fuelTankSize, string manufacture, string number)
+        {
+            FlightRange = flightRange;
+            FuelTankSize = fuelTankSize;
+            Manufacture = manufacture;
+            Number = number;
         }
 
         public abstract bool PrepareForFlight(IFlightPreparer preparer);        
