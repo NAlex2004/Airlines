f0b9eeab57b11647fb99c296d60ac0b0480ad832
alien <NAlex2004@gmail.com>
2017-04-18 20:54:24 +0300

    AirlineFactory some outlines

--- AirlineDemo/Factories/AirlineFactory.cs
+++ AirlineDemo/Factories/AirlineFactory.cs
@@ -7,9 +7,35 @@ namespace AirlineDemo.Factories
 {
     public class AirlineFactory: IAirlineFactory
     {
+        private DirectoryInfo airlineDirectory;
+        private string filePattern;
+
+        public AirlineFactory(string airlineDirectoryPath, string planesFilePattern)
+        {
+            airlineDirectory = new DirectoryInfo(airlineDirectoryPath);
+            filePattern = planesFilePattern;
+        }
+
         public ICollection<IPlane> CreateAirlinePlanes()
         {
-            throw new NotImplementedException();
+            List<IPlane> planes = new List<IPlane>();
+            var files = airlineDirectory.EnumerateFiles(filePattern);
+            foreach (var file in files)
+            {
+                string[] content = File.ReadAllLines(file.FullName);
+                IPlane plane = CreatePlane(content);
+                if (plane != null)
+                    planes.Add(plane);
+            }
+            return planes;
+        }
+
+        private IPlane CreatePlane(string[] config)
+        {
+            IPlane plane = null;
+
+
+            return plane;
         }
     }
 }
